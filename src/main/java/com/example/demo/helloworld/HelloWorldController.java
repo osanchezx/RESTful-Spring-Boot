700e52e5332ce5a1b5dfe89393816a3d7c7ba5ba
osanchezx <osanchezx@minsait.com>
2021-12-26 21:11:28 -0600

    Se agrega intenacionalizacion, hal explorer y actuator

--- src/main/java/com/example/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/example/demo/helloworld/HelloWorldController.java
@@ -1,12 +1,19 @@
 package com.example.demo.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller: maneja solicitude http
 @RestController
 public class HelloWorldController {
+	@Autowired
+	private MessageSource messageSource;
 	
 	//GET
 	//URI /hello-world
@@ -26,4 +33,17 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello world, %s", name));
 	}
+	
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("buenos.dias.mensaje", null, locale);
+	}
+	
+	/*
+	 * Alternativa del maeno de locale del anterior metodo
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("buenos.dias.mensaje", null, "Lenguaje no encontrado, mensaje por defecto", LocaleContextHolder.getLocale());
+	}
+	*/
 }
--- src/main/java/com/example/demo/user/UserResource.java
+++ src/main/java/com/example/demo/user/UserResource.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,14 +30,19 @@ public class UserResource {
 	}
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id){
+	public EntityModel<User> retrieveUser(@PathVariable int id){
 		User user =  service.findOne(id);
 		
 		if(user == null) {
 			throw new UserNotFoundException("id-"+id);
 		}
 		
-		return user;
+		EntityModel<User> model = EntityModel.of(user);
+		
+		WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		model.add(linkToUsers.withRel("all-users"));
+		
+		return model;
 	}
 	
 	@PostMapping("/users")
